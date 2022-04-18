566deebdb792cde9c6a3a37c73e2cf9def78d407
Shreya Sanghi <79101987+shreya017@users.noreply.github.com>
2022-04-19 01:30:08 +0530

    Add files via upload

---
+++ restaurent/about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BARISTA</title>
+        <link rel="stylesheet" herf="css/style.css">
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Capriola' rel='stylesheet'>
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-#890F0D">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">barista logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">STORE LOCATION</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">FRANCHISE WITH US</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">LOGIN/REGISTER</a>
+        </li>
+
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>BARISTA</h1>
+  <p>BARISTA WELCOMES YOU!</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">OUR STORY</h2>
+    <p class="py-3">Established in year 2000, Barista Coffee Company is the pioneer of coffee culture in India. The Barista Café chain of espresso bars delivers a truly Italian coffee experience in warm, friendly and relaxed environment. It provides a comfortable place for people to unwind over interesting conversations and a hot cup of coffee. The Barista Coffee Company Limited has over 225 Barista Coffee Espresso Cafe and Barista Coffee Kiosks across India including its Franchise partners. Besides India, Barista Coffee also has cafés in locations across Myanmar and Nepal.</p>
+    <a herf="about.php" class="btn btn-success"> know more </a>
+    <br>
+    </br>
+  </div>
+
+</section> 
+<footer>
+  <p class="p-3 bg-dark text-white text-center">Barista Coffee Company Limited.All Right Reserved</p>
+</footer> 
+
+</body>
+
+</html>
---
+++ restaurent/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Capriola' rel='stylesheet'>
+        <title>BARISTA</title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" herf="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Capriola' rel='stylesheet'>
+
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-#890F0D">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">barista logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">STORE LOCATION</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">FRANCHISE WITH US</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">LOGIN/REGISTER</a>
+        </li>
+
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/barista 1.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/barista 2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/barista 3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/barista 4.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/barista 5.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/barista 6.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">OUR STORY</h2>
+    <p class="detail">Established in year 2000, Barista Coffee Company is the pioneer of coffee culture in India. The Barista Café chain of espresso bars delivers a truly Italian coffee experience in warm, friendly and relaxed environment. It provides a comfortable place for people to unwind over interesting conversations and a hot cup of coffee. The Barista Coffee Company Limited has over 225 Barista Coffee Espresso Cafe and Barista Coffee Kiosks across India including its Franchise partners. Besides India, Barista Coffee also has cafés in locations across Myanmar and Nepal.</p>
+    <a herf="about.php">know more </a>
+    <br>
+    </br>
+  </div>
+
+</section>    
+        
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> 
+    </body>
+</html>            
